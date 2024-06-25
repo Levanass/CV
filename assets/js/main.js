ce87697b54af055c9ccb2f53e26d8a7aa4bd14c8
Levanass <106548224+Levanass@users.noreply.github.com>
2024-06-25 00:44:26 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,29 +133,7 @@
 				});
 
 
-// traduccion
-	// Inicializar i18next
-i18next.use(i18nextHttpBackend).init({
-  fallbackLng: 'en',
-  debug: true,
-  backend: {
-    loadPath: '/locales/{{lng}}.json' // Ruta a tus archivos de traducción
-  }
-}, function(err, t) {
-  // initialized and ready to go!
-  updateContent();
-});
-		// Función para cambiar de idioma
-function changeLanguage(lng) {
-  i18next.changeLanguage(lng, () => {
-    updateContent();
-  });
-}
-	
-// Función para actualizar el contenido traducido
-function updateContent() {
-  document.getElementById('presentacion').innerText = i18next.t('presentacion'); 
-}
+
 
 
 
